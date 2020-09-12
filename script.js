17d0fbdd693c0f9084d39dbfb0d2f3dcced87537
Stanley-Ugo <iamKosmicblast@gmail.com>
2020-09-12 06:03:55 +0100

    reading user-files & displaying it in the canvas

--- script.js
+++ script.js
@@ -1,10 +1,21 @@
-const canvas = document.getElementById('canvas');
+const canvas = document.getElementById("canvas");
 const ctx = canvas.getContext("2d");
 
 const img = new Image();
-img.src = 'Horizon.jpg';
-img.onload = () => {
-    canvas.width = img.width;
-    canvas.height = img.height;
-    ctx.drawImage(img, 0, 0);
+
+const reader = new FileReader();
+
+function uploadImage(e) {
+  reader.readAsDataURL(e.target.files[0]);
+  reader.onload = () => {
+    img.src = reader.result;
+    img.onload = () => {
+      canvas.width = img.width;
+      canvas.height = img.height;
+      ctx.drawImage(img, 0, 0);
+    };
+  };
 }
+
+const imageLoader = document.getElementById("uploader");
+imageLoader.addEventListener("change", uploadImage);
